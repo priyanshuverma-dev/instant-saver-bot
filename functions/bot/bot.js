e80fa8d0a3c2fa41acdda063e6b253ec6e6ca741
priyanshu-creator <priyanshu.creator.dev@gmail.com>
2023-01-29 22:23:31 +0530

    FIXED24 FOR DEPLOY

--- functions/bot/bot.js
+++ functions/bot/bot.js
@@ -108,19 +108,7 @@ bot.on("message", (ctx) => {
 });
 // bot.launch();
 // AWS event handler syntax (https://docs.aws.amazon.com/lambda/latest/dg/nodejs-handler.html)
-exports.handler = async function handler(event) {
-  try {
-    await bot.handleUpdate(JSON.parse(event.body));
-    return { statusCode: 200, body: "" };
-  } catch (e) {
-    console.error("error in handler:", e);
-    return {
-      statusCode: 400,
-      body: "This endpoint is meant for bot and telegram communication",
-    };
-  }
-};
-// export async function handler(event) {
+// exports.handler = async function handler(event) {
 //   try {
 //     await bot.handleUpdate(JSON.parse(event.body));
 //     return { statusCode: 200, body: "" };
@@ -131,4 +119,16 @@ exports.handler = async function handler(event) {
 //       body: "This endpoint is meant for bot and telegram communication",
 //     };
 //   }
-// }
+// };
+export async function handler(event) {
+  try {
+    await bot.handleUpdate(JSON.parse(event.body));
+    return { statusCode: 200, body: "" };
+  } catch (e) {
+    console.error("error in handler:", e);
+    return {
+      statusCode: 400,
+      body: "This endpoint is meant for bot and telegram communication",
+    };
+  }
+}
