50d911b56b378f356f57fd2aa1f5ed179f0386d2
priyanshu-creator <priyanshu.creator.dev@gmail.com>
2023-01-29 18:18:43 +0530

    FIXED 11 FOR DEPLOY

--- functions/bot/index.js
+++ functions/bot/index.js
@@ -2,12 +2,12 @@
 // import fetch from "node-fetch";
 // import { Telegraf } from "telegraf";
 // import * as dotenv from "dotenv";
-
+import("node-fetch");
 const { Telegraf } = require("telegraf");
 require("dotenv").config();
-fetch = require("node-fetch");
+// fetch = require("node-fetch");
 
-dotenv.config();
+// dotenv.config();
 const bot = new Telegraf(process.env.BOT_TOKEN);
 const API_URL = "https://priyanshu-vid-api.onrender.com";
 // command Start
